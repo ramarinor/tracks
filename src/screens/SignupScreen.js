c0c13e8cdde42fd4d35f93f21f14fffadff4a98e
ramarinor <rinor.rama95@gmail.com>
2022-05-17 16:26:15 +0200

    created reusable components

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,51 +1,24 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../ components/spacer';
+import AuthForm from '../ components/AuthForm';
+import NavLink from '../ components/NavLink';
 import { Context as AuthContext } from '../context/authContext';
 
 const SingupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  console.log;
+
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign up for trakcer</Text>
-      </Spacer>
-      <Spacer>
-        <Input
-          label="Email"
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-      </Spacer>
-      <Spacer>
-        <Input
-          label="Password"
-          value={password}
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry
-        />
-      </Spacer>
-
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-
-      <Spacer>
-        <Button
-          title="Sign up"
-          onPress={() => {
-            signup({ email, password });
-          }}
-        />
-      </Spacer>
+      <AuthForm
+        headerText="Sign up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
+      />
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? Sign in instead!"
+      />
     </View>
   );
 };
@@ -59,12 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 200
-  },
-  errorMessage: {
-    fontSize: 16,
-    color: 'red',
-    marginLeft: 15,
-    marginTop: 15
   }
 });
 
