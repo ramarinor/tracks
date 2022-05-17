44fdc895c7d0cceb020b47930362ac8682fec7d5
ramarinor <rinor.rama95@gmail.com>
2022-05-17 11:53:02 +0200

    first commit

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SingupScreen = () => {
+  return <Text style={{ fontSize: 48 }}>SingupScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SingupScreen;
