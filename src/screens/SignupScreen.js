6a5cb33e01c6bab25d0b372735db5c07e8c51e18
ramarinor <rinor.rama95@gmail.com>
2022-05-17 12:25:01 +0200

    initial routes

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SingupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SingupScreen</Text>;
+const SingupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => {
+          navigation.navigate('Signin');
+        }}
+      />
+      <Button
+        title="Go to mainFLow"
+        onPress={() => {
+          navigation.navigate('mainFlow');
+        }}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
