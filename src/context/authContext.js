3f02fc51f70bc51db21b5029af576ed44fd4a4d2
ramarinor <rinor.rama95@gmail.com>
2022-05-18 09:46:22 +0200

    added signout

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
       return { token: action.payload, errorMessage: '' };
     case 'clear_error_message':
       return { ...state, errorMessage: '' };
+    case 'signout':
+      return { token: null, errorMessage: '' };
     default:
       return state;
   }
@@ -63,8 +65,10 @@ const signin = (dispatch) => {
   };
 };
 
-const signout = (dispatch) => {
-  return () => {};
+const signout = (dispatch) => async () => {
+  await AsyncStorage.removeItem('token');
+  dispatch({ type: 'signout' });
+  navigate('Signup');
 };
 
 export const { Provider, Context } = createDataContext(
